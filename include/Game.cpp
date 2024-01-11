c3bd12aa5dd352dca6929b279230b8ee8faf1dfd
gon2santos <necchigonzalo@gmail.com>
2024-01-11 20:37:07 -0300

    separate shader files

--- include/Game.cpp
+++ include/Game.cpp
@@ -85,11 +85,11 @@ void Game::Update()
 void Game::Render()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-
-    /* int windowWidth, windowHeight;
-    SDL_GetWindowSize(window, &windowWidth, &windowHeight); */
-
-    glUseProgram(shaderProgram);
+    // float elapsedTime = SDL_GetTicks() / 1000.0; // elapsed time in seconds since sdl init
+    // float greenValue = (sin(elapsedTime) / 2.0f) + 0.5f;
+    // int vertexColorLocation = glGetUniformLocation(shaderProgram, "dnmcolor");
+    shaderProgram->use();
+    // glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
     glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
@@ -121,50 +121,11 @@ void Game::InitBufferObjectsAndSetupShaders()
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    shaderProgram = new Shader("./shaders/vertexShader.vs", "./shaders/fragmentShader.fs");
 
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
-                  << infoLog << std::endl;
-    }
-
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
-                  << infoLog << std::endl;
-    }
-
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success)
-    {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
-                  << infoLog << std::endl;
-    }
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-    glEnableVertexAttribArray(0);
+    // glVertexAttribPointer(indice, cant de componentes por atributo, tipo del componente, normalizado?, stride, offset)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);                   // position attibute
+    glEnableVertexAttribArray(0);                                                                    // habilitar este atributo (pos)
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float))); // color attibute
+    glEnableVertexAttribArray(1);                                                                    // habilitar este atributo (color)
 }
--- include/Game.hpp
+++ include/Game.hpp
@@ -4,6 +4,7 @@
 #include "glad/glad.h"
 #include <SDL2/SDL.h>
 #include <iostream>
+#include "Shader.hpp"
 
 #define SCREEN_WIDTH 600
 #define SCREEN_HEIGHT 400
@@ -35,30 +36,33 @@ public:
         KEY_PRESS_SURFACE_TOTAL
     };
 
-    float vertices[9] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f};
+    float vertices[18] = {
+        // positions         // colors
+        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
+        0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f    // top
+    };
     unsigned int VBO;
     unsigned int VAO;
     unsigned int vertexShader;
     unsigned int fragmentShader;
-    unsigned int shaderProgram;
+    Shader *shaderProgram;
 
     const char *vertexShaderSource = "#version 330 core\n"
                                      "layout (location = 0) in vec3 aPos;\n"
-                                     "out vec4 vertexcolor;\n"
+                                     "layout (location = 1) in vec3 aColor;\n"
+                                     "out vec3 ourColor;\n"
                                      "void main()\n"
                                      "{\n"
-                                     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                                     "   vertexcolor = vec4(0.2f, 0.0f, 0.3f, 0.1f);\n"
+                                     "   gl_Position = vec4(aPos, 1.0);\n"
+                                     "   ourColor = aColor;\n"
                                      "}\0";
     const char *fragmentShaderSource = "#version 330 core\n"
+                                       "in vec3 ourColor;\n"
                                        "out vec4 FragColor;\n"
-                                       "in vec4 vertexcolor;\n"
                                        "void main()\n"
                                        "{\n"
-                                       "FragColor = vertexcolor;\n"
+                                       "FragColor = vec4(ourColor, 1.0f);\n"
                                        "}\0";
 
 private:
